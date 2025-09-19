45a7b985a0fe4276f2fb7e75b72ea88f1307613b
Plamena <plamena@alphabite.io>
2025-09-19 22:00:18 +0300

    Use hash router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,61 @@
-import { createBrowserRouter, Navigate, RouteObject } from 'react-router'
+import {
+  // createBrowserRouter,
+  Navigate,
+  RouteObject,
+  createHashRouter,
+} from 'react-router'
 import { ROUTES } from '@/router/constants/app-routes'
 import DefaultLayout from '@/layouts/default'
 import CardPage from '@/card/card.page'
 import CardsPage from '@/cards/cards.page'
 import MyCardsPage from '@/cards/my-cards.page'
+
+// export const routes: RouteObject[] = [
+//   {
+//     element: <DefaultLayout />,
+//     children: [
+//       {
+//         path: ROUTES.HOME,
+//         index: true,
+//         element: <Navigate to={ROUTES.CARDS} />,
+//       },
+//       {
+//         path: ROUTES.CARDS,
+//         index: true,
+//         element: <CardsPage />,
+//         // errorElement: <GeneralError />,
+//       },
+//       {
+//         path: ROUTES.CARD,
+//         index: true,
+//         element: <CardPage />,
+//         // errorElement: <GeneralError />,
+//       },
+//       {
+//         path: ROUTES.MY_CARDS,
+//         index: true,
+//         element: <MyCardsPage />,
+//         // errorElement: <GeneralError />,
+//       },
+//       {
+//         path: ROUTES.MY_CARD,
+//         index: true,
+//         element: <CardPage />,
+//         // errorElement: <GeneralError />,
+//       },
+//       {
+//         path: '*',
+//         element: <div>Not Found</div>,
+//       },
+//     ],
+//   },
+//   {
+//     path: '*',
+//     element: <div>Not Found</div>,
+//   },
+// ]
+
+// export default createBrowserRouter(routes)
 
 export const routes: RouteObject[] = [
   {
@@ -14,40 +66,14 @@ export const routes: RouteObject[] = [
         index: true,
         element: <Navigate to={ROUTES.CARDS} />,
       },
-      {
-        path: ROUTES.CARDS,
-        index: true,
-        element: <CardsPage />,
-        // errorElement: <GeneralError />,
-      },
-      {
-        path: ROUTES.CARD,
-        index: true,
-        element: <CardPage />,
-        // errorElement: <GeneralError />,
-      },
-      {
-        path: ROUTES.MY_CARDS,
-        index: true,
-        element: <MyCardsPage />,
-        // errorElement: <GeneralError />,
-      },
-      {
-        path: ROUTES.MY_CARD,
-        index: true,
-        element: <CardPage />,
-        // errorElement: <GeneralError />,
-      },
-      {
-        path: '*',
-        element: <div>Not Found</div>,
-      },
+      { path: ROUTES.CARDS, element: <CardsPage /> },
+      { path: ROUTES.CARD, element: <CardPage /> },
+      { path: ROUTES.MY_CARDS, element: <MyCardsPage /> },
+      { path: ROUTES.MY_CARD, element: <CardPage /> },
+      { path: '*', element: <div>Not Found</div> },
     ],
   },
-  {
-    path: '*',
-    element: <div>Not Found</div>,
-  },
+  { path: '*', element: <div>Not Found</div> },
 ]
 
-export default createBrowserRouter(routes)
+export default createHashRouter(routes)
